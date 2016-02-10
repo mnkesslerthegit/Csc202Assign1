6509d2813bc1efe873818767f72d55eb9eaf3e61
mnk2060 <mnk2060@AN116770-CT116.nvstu.edu>
2016-02-09 20:14:06 -0500

    First attempt in 202

---
+++ BigDecimal/src/BigDecimal.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+
+public class BigDecimal {
+	// this list stores as many digits as needed
+	private ArrayList<Integer> myList = new ArrayList<>();
+
+	public BigDecimal(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			{
+				myList.add(Integer.parseInt("" + input.charAt(i)));
+			}
+		}
+	}
+
+	/**
+	 * Creates a backwards version of an arrayList<Integer>
+	 * 
+	 * @param list
+	 * @return
+	 */
+	private ArrayList<Integer> reverseList(ArrayList<Integer> list) {
+		ArrayList<Integer> result = new ArrayList<>();
+		for (int i = list.size() - 1; i >= 0; i--) {
+			result.add(list.get(i));
+		}
+		return result;
+	}
+
+	/**
+	 * Here I simulate doing addition by hand.
+	 * 
+	 * @param bd2
+	 * @return
+	 */
+	public BigDecimal add(BigDecimal bd2) {
+		BigDecimal small = smaller(bd2);
+		BigDecimal large = larger(bd2);
+		ArrayList<Integer> rA = reverseList(small.myList);
+		ArrayList<Integer> rB = reverseList(large.myList);
+		String result = "";
+
+		int overflow = 0;
+		int i = 0;
+		while (i < small.myList.size()) {
+			int sum = rA.get(i) + rB.get(i) + overflow;
+			overflow = sum / 10;
+			result = (sum % 10) + result;
+			i++;
+
+		}
+
+		while (i < large.myList.size()) {
+			int sum = rB.get(i) + overflow;
+			overflow = sum / 10;
+			result = (sum % 10) + result;
+			i++;
+		}
+		if (overflow > 0) {
+			result = overflow + result;
+		}
+		return new BigDecimal(result);
+
+	}
+
+	public String toString() {
+		return myList.toString();
+	}
+
+	/**
+	 * Check to see if one big decimal is greater than another
+	 * 
+	 * @param other
+	 * @return
+	 */
+	public boolean greaterThan(BigDecimal other) {
+		if (other.myList.size() < myList.size()) {
+			return true;
+		} else if (other.myList.size() > myList.size()) {
+			return false;
+
+		}
+
+		for (int i = 0; i < other.myList.size(); i++) {
+			if (myList.get(i) > other.myList.get(i)) {
+				return true;
+			} else if (myList.get(i) < other.myList.get(i)) {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * returns the larger bigDecimal
+	 * 
+	 * @param other
+	 * @return
+	 */
+	private BigDecimal larger(BigDecimal other) {
+		if (greaterThan(other)) {
+			return this;
+		} else {
+			return other;
+		}
+
+	}
+
+	/**
+	 * returns the smaller bigDecimal
+	 * 
+	 * @param other
+	 * @return
+	 */
+	private BigDecimal smaller(BigDecimal other) {
+		if (greaterThan(other)) {
+			return other;
+		} else {
+			return this;
+		}
+
+	}
+
+	public BigDecimal subtract(BigDecimal bd2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BigDecimal divide(BigDecimal bd2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BigDecimal multiply(BigDecimal bd2) {
+
+		return null;
+	}
+
+}
---
+++ BigDecimal/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scan = new Scanner(System.in);
+
+	/**
+	 * Allows user to enter two numbers of any size, A and B The program will
+	 * then print the sum, difference, product, quotien of A and B.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		BigDecimal bdA;
+		BigDecimal bdB;
+		
+	
+		 //check if user is trying to use command line arguments		 
+		if (args.length == 2) {
+			bdA = new BigDecimal(args[1]);
+			bdB = new BigDecimal(args[2]);
+		} else { // if not using arguments, prompt user for numbers
+			String input = "";
+			//prompt for first number
+			do {
+				System.out.println("Enter number 1");
+				input = scan.nextLine();
+			} while (!checkFormat(input));
+			bdA = new BigDecimal(input);
+			//prompt for second number
+			input = "";
+			do {
+				System.out.println("Enter number 2");
+				input = scan.nextLine();
+			} while (!checkFormat(input));
+			bdB = new BigDecimal(input);
+
+			//do operations and print results
+			printOperations(bdA,bdB);
+
+		}
+	}
+
+	/**
+	 * Determines if the user input is the correct format
+	 * 
+	 * @param input
+	 * @return
+	 */
+	private static boolean checkFormat(String input) {
+
+		for (int i = 0; i < input.length(); i++) {
+			if (Character.isAlphabetic(input.charAt(i))) {
+				System.out.println("Bad input: try again");
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * The print method which displays the result of each operation
+	 */
+	private static void printOperations(BigDecimal bd1, BigDecimal bd2) {
+		System.out.println(bd1.add(bd2));
+	//	System.out.println(bd1.subtract(bd2));
+	//	System.out.println(bd1.divide(bd2));
+	//	System.out.println(bd1.multiply(bd2));
+	}
+
+}
